b1faaef0f055b6d95022c199e9c35a308ff914e3
ChaeyeonHan <71596178+ChaeyeonHan@users.noreply.github.com>
2024-08-10 15:32:36 +0900

    [Bronze I] Title: 나무 조각, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/2947. 나무 조각/나무 조각.py
@@ -0,0 +1,10 @@
+import sys
+input = sys.stdin.readline
+
+nums = list(map(int, input().split()))
+
+for i in range(len(nums)-1, 0, -1):
+    for j in range(i):
+        if nums[j] > nums[j+1]:
+            nums[j], nums[j+1] = nums[j+1], nums[j]
+            print(*nums)
