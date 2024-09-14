c03434925dfd21754699dc7644d12a35e954f584
ChaeyeonHan <71596178+ChaeyeonHan@users.noreply.github.com>
2024-09-14 12:33:57 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 48 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
@@ -3,26 +3,24 @@ input = sys.stdin.readline
 
 N = int(input())
 nums = list(map(int, input().split()))
-add, sub, mul, div = map(int, input().split())
-
-# 방법1: DFS 이용
-max_result = -1e9
-min_result = 1e9
+add, sub, mul, div = map(int, input().split())  # 덧셈/뺄셈/곱셈/나눗셈
+max_result = -int(1e9)
+min_result = int(1e9)
 
 def dfs(depth, total, add, sub, mul, div):
     global max_result, min_result
-    if depth == N:  # 모든 연산 완료
-        max_result = max(total, max_result)
-        min_result = min(total, min_result)
+    if depth == N:
+        max_result = max(max_result, total)
+        min_result = min(min_result, total)
         return
     if add:
-        dfs(depth+1, total + nums[depth], add-1, sub, mul, div)
+        dfs(depth + 1, total + nums[depth], add-1, sub, mul, div)
     if sub:
-        dfs(depth+1, total - nums[depth], add, sub-1, mul, div)
+        dfs(depth + 1, total - nums[depth], add, sub-1, mul, div)
     if mul:
-        dfs(depth+1, total * nums[depth], add, sub, mul-1, div)
+        dfs(depth + 1, total * nums[depth], add, sub, mul - 1, div)
     if div:
-        dfs(depth+1, int(total / nums[depth]), add, sub, mul, div-1)
+        dfs(depth + 1, int(total / nums[depth]), add, sub, mul, div - 1)
 
 dfs(1, nums[0], add, sub, mul, div)
 
