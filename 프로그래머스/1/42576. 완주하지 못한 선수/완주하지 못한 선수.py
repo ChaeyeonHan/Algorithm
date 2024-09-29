5055c37207b84434e2b6b1de0cc8afe26d3aa51e
ChaeyeonHan <71596178+ChaeyeonHan@users.noreply.github.com>
2024-09-29 18:39:15 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 56.39 ms, Memory: 34 MB -BaekjoonHub

---
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -0,0 +1,12 @@
+# 해시 : 이름을 키로 사용, 동명이인의 수를 값
+def solution(participant, completion):
+    d={}  # 빈사전
+    for i in participant:
+        d[i] = d.get(i, 0) + 1  # 주어진 키 i가 존재하면 그 값을, 없으면 0(디폴트값)을
+        # 처음 등장하면 0+1, 동명이인이라면 기존인원수+1
+
+    for j in completion:
+        d[j] -= 1  # 이름 한명씩 빼주기
+    result = [k for k, v in d.items() if v>0]  # value가 1인 선수 찾기
+    answer = result[0]
+    return answer
