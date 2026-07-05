83aff70abe77f21ad371cce28e953990a2eef34e
ChaeyeonHan <71596178+ChaeyeonHan@users.noreply.github.com>
2026-07-05 14:27:18 +0900

    [level 2] Title: 주식가격, Time: 64.69 ms, Memory: 17 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.py
+++ 프로그래머스/2/42584. 주식가격/주식가격.py
@@ -1,14 +1,29 @@
-from collections import deque
+# from collections import deque
 
+# def solution(prices):
+#     answer = []
+#     queue = deque(prices)
+#     while queue:
+#         price = queue.popleft()
+#         sec = 0
+#         for next in queue:
+#             sec += 1
+#             if price > next:
+#                 break
+#         answer.append(sec)
+#     return answer
+
+
+from collections import deque
 def solution(prices):
     answer = []
     queue = deque(prices)
     while queue:
-        price = queue.popleft()
-        sec = 0
+        cur = queue.popleft()
+        time = 0
         for next in queue:
-            sec += 1
-            if price > next:
+            time += 1
+            if cur > next:  # 가격 떨어짐
                 break
-        answer.append(sec)
+        answer.append(time)
     return answer
